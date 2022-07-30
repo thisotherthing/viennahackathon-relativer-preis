dfaad5221199941b7d17e8424f0ca56b8f570bd7
Stefan Kernjak <stefan@thisotherthing.co>
2022-07-30 21:25:35 +0200

    items, added hover

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -16,24 +16,27 @@ export const Item = (props: Props) => {
   if (props.item.federalStateFilter !== undefined && props.item.federalStateFilter !== federalState) return null;
 
   return (
-    <section tw="flex flex-row items-center border-radius[30px] border border-black p-16 pr-28 mb-20 w-full md:width[calc(50% - 10px)] font-bold">
-      <div
-        tw="aspect-ratio[1] width[100px] border-radius[20px] overflow-hidden mr-26 flex-shrink-0 flex justify-center items-center"
-        style={{ background: `#${props.item.color}` || "green" }}
-      >
-        <img tw="w-60" src={`/icons/${props.item.icon}.svg`} alt={`${props.item.name} icon`} aria-hidden />
-      </div>
+    <section tw="font-bold relative w-full md:width[calc(50% - 10px)] mb-20">
+      <div tw="border-radius[26px] border-2 border-dashed border-black w-full h-full absolute transform[scale(0.99)]" />
+      <div tw="flex flex-row items-center transform hover:-translate-x-7 hover:-translate-y-7 border-radius[26px] border-2 border-black p-16 pr-28 w-full background[white] transition transition-transform">
+        <div
+          tw="aspect-ratio[1] width[100px] border-radius[20px] overflow-hidden mr-26 flex-shrink-0 flex justify-center items-center"
+          style={{ background: `#${props.item.color}` || "green" }}
+        >
+          <img tw="w-60" src={`/icons/${props.item.icon}.svg`} alt={`${props.item.name} icon`} aria-hidden />
+        </div>
 
-      <div tw="flex flex-col w-full truncate">
-        <h3 tw="truncate mb-8 font-size[18px]">
-          <a tw="underline" target="_blank" rel="noopener noreferrer" href={props.item.url}>
-            {props.item.name}
-          </a>
-        </h3>
-        <div tw="w-full flex flex-row justify-between font-size[22px]">
-          <span>{props.item.price} €</span>
-          <span>{"-->"}</span>
-          <span>{adjusted.adjustedPrice.toFixed(2)} €</span>
+        <div tw="flex flex-col w-full truncate">
+          <h3 tw="truncate mb-8 font-size[18px]">
+            <a tw="underline" target="_blank" rel="noopener noreferrer" href={props.item.url}>
+              {props.item.name}
+            </a>
+          </h3>
+          <div tw="w-full flex flex-row justify-between font-size[22px]">
+            <span>{props.item.price} €</span>
+            <span>{"-->"}</span>
+            <span>{adjusted.adjustedPrice.toFixed(2)} €</span>
+          </div>
         </div>
       </div>
     </section>
