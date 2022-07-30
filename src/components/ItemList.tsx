1e49e0828904fcb2b546af34f8cf7d6149bab2b0
Hossaini Hosain <Hossainih03@gmail.com>
2022-07-30 16:26:37 +0200

    4

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const ItemList = (props: Props) => {
   return (
-    <table tw="m-auto mt-30 mb-80">
+    <table tw="w-full mt-30 mb-80 border-t-2">
       {props.items.map((item, i) => (
         <Item item={item} key={`${item.name} ${i}`} />
       ))}
