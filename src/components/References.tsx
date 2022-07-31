db4ffb8266a73ea8196a4784771615ac92e01ffe
Stefan Kernjak <stefan@thisotherthing.co>
2022-07-31 10:15:42 +0200

    added repo link

--- src/components/References.tsx
+++ src/components/References.tsx
@@ -2,7 +2,7 @@ import { references } from "~/assets/data";
 
 export const References = () => {
   return (
-    <div tw="w-full text-left pb-80">
+    <div tw="w-full text-left pb-80 flex flex-col">
       <div>Links:</div>
       {references.map((link) => (
         <a target="_blank" rel="noopener noreferrer" href={link.url} tw="underline" key={link.url}>
